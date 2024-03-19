e41af7e6859e480e7ae0248873068a3713b65f3a
Rafael <Rafael@gmail.com>
2024-03-19 09:09:52 -0300

    Correção da puta

--- Exemplos JS/Aula 12-03/exemplo07.js
+++ Exemplos JS/Aula 12-03/exemplo07.js
@@ -1,6 +1,52 @@
-calcularIMC(55, 1.60);
-
 function calcularIMC(locPeso, locAltura) {
     let imc = locPeso / (locAltura ** 2);
     imc = locPeso / (locAltura ** 2);
+    resultadoIMC = imc.toFixed(2);
+
+    //Abaixo de 18.5
+    if (imc < 18.5) {
+        console.log("Seu peso é de: " + locPeso);
+        console.log("Sua altura é de: " + locAltura);
+        console.log("Seu IMC é de: " + resultadoIMC + " - Você está com Desnutrição");
+    } 
+
+    //Entre 18.5 e 25
+    else if (imc >= 18.5 && imc < 25) {
+        console.log("Seu peso é de: " + locPeso);
+        console.log("Sua altura é de: " + locAltura);
+        console.log("Seu IMC é de: " + resultadoIMC + " - Você está com Peso normal");
+    } 
+
+    //Entre 25 e 30
+    else if (imc >= 25 && imc < 30) {
+        console.log("Seu peso é de: " + locPeso);
+        console.log("Sua altura é de: " + locAltura);
+        console.log("Seu IMC é de: " + resultadoIMC + " - Você está com Sobrepeso");
+    } 
+
+    //Entre 30 e 35
+    else if (imc >= 30 && imc < 35) {
+        console.log("Seu peso é de: " + locPeso);
+        console.log("Sua altura é de: " + locAltura);
+        console.log("Seu IMC é de: " + resultadoIMC + " - Você está com Obesidade I");
+    } 
+
+    //Entre 35 e 40
+    else if (imc >= 35 && imc < 40) {
+        console.log("Seu peso é de: " + locPeso);
+        console.log("Sua altura é de: " + locAltura);
+        console.log("Seu IMC é de: " + resultadoIMC + " - Você está com Obesidade II");
+    } 
+
+    //Acima de 40
+    else if (imc >= 40) {
+        console.log("Seu peso é de: " + locPeso);
+        console.log("Sua altura é de: " + locAltura);
+        console.log("Seu IMC é de: " + resultadoIMC + " - Você está com Obesidade III");
+    } 
+
+    else {
+        console.log("Insira um valor válido");
+    }
+}
     resultadoIMC = imc.toFixed(2);
